24bff44a9d67b6699ee65592b428e66191327060
Nguyễn Việt Hoàn <95644536+viethoan261@users.noreply.github.com>
2023-03-26 10:37:07 +0700

    fix cors

--- src/main/java/com/example/hotel/security/controller/AuthController.java
+++ src/main/java/com/example/hotel/security/controller/AuthController.java
@@ -7,15 +7,13 @@ import com.example.hotel.security.serivce.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/auth")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AuthController {
     @Autowired
     private AuthService service;
